6c2b1c3b022129aec754119f29f2d19a5bc54069
Christopher Jimmy <christopherjimmy999@gmail.com>
2025-11-08 14:52:13 -0500

    simplied

--- property.py
+++ property.py
@@ -1,5 +1,5 @@
 class property:
-    #state: house or hotel
+    #state: house or hotel or railroad
     #color: has different # or properties for monopoly or rent depends on color
     propertyColors = ["brown", "light blue", "pink", "orange", "red", "yellow", "green", "dark blue"]
     propertyName = ["Baird", "Clements", "Baldy", "Jacobs", "Park","O'Brian","Norton","Knox","Bonner","Talbert","Hochstetter","Cooke","Fronczak","NSC","Ketter","Slee","Furnas","The Commons","Center for the Arts","Alumni Arena", "Davis", "Jarvis"]
@@ -11,8 +11,7 @@ class property:
 
     def rent(self):
         if self.state == "house":
-            if self.color == property.propertyColors[0]: #brown
-                if self.name == property.propertyName[0]: #Baird
+            if self.name == property.propertyName[0]: #Baird
                     if self.buildings == 0:
                         return 2
                     elif self.buildings == 1:
@@ -23,7 +22,7 @@ class property:
                         return 90
                     elif self.buildings == 4:
                         return 160
-                elif self.name == property.propertyName[1]: #Clements
+            elif self.name == property.propertyName[1]: #Clements
                     if self.buildings == 0:
                         return 4
                     elif self.buildings == 1:
@@ -34,8 +33,7 @@ class property:
                         return 180
                     elif self.buildings == 4:
                         return 320
-            elif self.color == property.propertyColors[1]: #light blue
-                if self.name == property.propertyName[2]: #Baldy
+            elif self.name == property.propertyName[2]: #Baldy
                     if self.buildings == 0:
                         return 6
                     elif self.buildings == 1:
@@ -46,7 +44,7 @@ class property:
                         return 270
                     elif self.buildings == 4:
                         return 400
-                elif self.name == property.propertyName[3]: #Jacobs
+            elif self.name == property.propertyName[3]: #Jacobs
                     if self.buildings == 0:
                         return 6
                     elif self.buildings == 1:
@@ -57,7 +55,7 @@ class property:
                         return 270
                     elif self.buildings == 4:
                         return 400
-                elif self.name == property.propertyName[4]: #Park
+            elif self.name == property.propertyName[4]: #Park
                     if self.buildings == 0:
                         return 8
                     elif self.buildings == 1:
@@ -68,8 +66,7 @@ class property:
                         return 300
                     elif self.buildings == 4:
                         return 450
-            elif self.color == property.propertyColors[2]: #pink
-                if self.name == property.propertyName[5]: #O'Brian
+            elif self.name == property.propertyName[5]: #O'Brian
                     if self.buildings == 0:
                         return 8
                     elif self.buildings == 1:
@@ -80,7 +77,7 @@ class property:
                         return 300
                     elif self.buildings == 4:
                         return 450
-                elif self.name == property.propertyName[6]: #Norton
+            elif self.name == property.propertyName[6]: #Norton
                     if self.buildings == 0:
                         return 8
                     elif self.buildings == 1:
@@ -91,7 +88,7 @@ class property:
                         return 300
                     elif self.buildings == 4:
                         return 450
-                elif self.name == property.propertyName[7]: #Knox
+            elif self.name == property.propertyName[7]: #Knox
                     if self.buildings == 0:
                         return 10
                     elif self.buildings == 1:
@@ -102,8 +99,7 @@ class property:
                         return 450
                     elif self.buildings == 4:
                         return 625
-            elif self.color == property.propertyColors[3]: #orange
-                if self.name == property.propertyName[8]: #Bonner
+            elif self.name == property.propertyName[8]: #Bonner
                     if self.buildings == 0:
                         return 10
                     elif self.buildings == 1:
@@ -114,7 +110,7 @@ class property:
                         return 450
                     elif self.buildings == 4:
                         return 625
-                elif self.name == property.propertyName[9]: #Talbert
+            elif self.name == property.propertyName[9]: #Talbert
                     if self.buildings == 0:
                         return 10
                     elif self.buildings == 1:
@@ -125,11 +121,18 @@ class property:
                         return 450
                     elif self.buildings == 4:
                         return 625
-                elif self.name == property.propertyName[10]: #Hochstetter
-                    # rents for Hochstetter not defined yet
-                    pass
-            elif self.color == property.propertyColors[4]: #red                  
-                if self.name == property.propertyName[11]: #Cooke
+            elif self.name == property.propertyName[10]: #Hochstetter
+                    if self.buildings == 0:
+                        return 12
+                    elif self.buildings == 1:
+                        return 60
+                    elif self.buildings == 2:
+                        return 180
+                    elif self.buildings == 3:
+                        return 500
+                    elif self.buildings == 4:
+                        return 700
+            elif self.name == property.propertyName[11]: #Cooke
                     if self.buildings == 0:
                         return 22
                     elif self.buildings == 1:
@@ -140,7 +143,7 @@ class property:
                         return 800
                     elif self.buildings == 4:
                         return 1000
-                elif self.name == property.propertyName[12]: #Fronczak
+            elif self.name == property.propertyName[12]: #Fronczak
                     if self.buildings == 0:
                         return 22
                     elif self.buildings == 1:
@@ -151,7 +154,7 @@ class property:
                         return 800
                     elif self.buildings == 4:
                         return 1000
-                elif self.name == property.propertyName[13]: #NSC
+            elif self.name == property.propertyName[13]: #NSC
                     if self.buildings == 0:
                         return 24
                     elif self.buildings == 1:
@@ -162,8 +165,7 @@ class property:
                         return 850
                     elif self.buildings == 4:
                         return 1050
-            elif self.color == property.propertyColors[5]: #yellow
-                if self.name == property.propertyName[14]: #Ketter
+            elif self.name == property.propertyName[14]: #Ketter
                     if self.buildings == 0:
                         return 24
                     elif self.buildings == 1:
@@ -174,7 +176,7 @@ class property:
                         return 850
                     elif self.buildings == 4:
                         return 1050
-                elif self.name == property.propertyName[15]: #Slee
+            elif self.name == property.propertyName[15]: #Slee
                     if self.buildings == 0:
                         return 26
                     elif self.buildings == 1:
@@ -185,7 +187,7 @@ class property:
                         return 900
                     elif self.buildings == 4:
                         return 1100
-                elif self.name == property.propertyName[16]: #Furnas
+            elif self.name == property.propertyName[16]: #Furnas
                     if self.buildings == 0:
                         return 26
                     elif self.buildings == 1:
@@ -196,8 +198,7 @@ class property:
                         return 900
                     elif self.buildings == 4:
                         return 1100
-            elif self.color == property.propertyColors[6]: #green
-                if self.name == property.propertyName[17]: #The Commons
+            elif self.name == property.propertyName[17]: #The Commons
                     if self.buildings == 0:
                         return 28
                     elif self.buildings == 1:
@@ -208,7 +209,7 @@ class property:
                         return 1000
                     elif self.buildings == 4:
                         return 1200
-                elif self.name == property.propertyName[18]: #Center for the Arts
+            elif self.name == property.propertyName[18]: #Center for the Arts
                     if self.buildings == 0:
                         return 28
                     elif self.buildings == 1:
@@ -219,7 +220,7 @@ class property:
                         return 1000
                     elif self.buildings == 4:
                         return 1200
-                elif self.name == property.propertyName[19]: #Alumni Arena
+            elif self.name == property.propertyName[19]: #Alumni Arena
                     if self.buildings == 0:
                         return 30
                     elif self.buildings == 1:
@@ -230,8 +231,7 @@ class property:
                         return 1100
                     elif self.buildings == 4:
                         return 1300
-            elif self.color == property.propertyColors[7]: #dark blue
-                if self.name == property.propertyName[20]: #Davis
+            elif self.name == property.propertyName[20]: #Davis
                     if self.buildings == 0:
                         return 35
                     elif self.buildings == 1:
@@ -242,7 +242,7 @@ class property:
                         return 1200
                     elif self.buildings == 4:
                         return 1400
-                elif self.name == property.propertyName[21]: #Jarvis
+            elif self.name == property.propertyName[21]: #Jarvis
                     if self.buildings == 0:
                         return 50
                     elif self.buildings == 1:
@@ -255,55 +255,47 @@ class property:
                         return 1700 
                                       
         elif self.state == "hotel":
-            if self.color == property.propertyColors[0]: #brown
                 if self.name == property.propertyName[0]: #Baird
                     return 250
                 elif self.name == property.propertyName[1]: #Clements
                     return 450
-            elif self.color == property.propertyColors[1]: #light blue
-                if self.name == property.propertyName[2]: #Baldy
+                elif self.name == property.propertyName[2]: #Baldy
                     return 550
                 elif self.name == property.propertyName[3]: #Jacobs
                     return 550
                 elif self.name == property.propertyName[4]: #Park
                     return 600
-            elif self.color == property.propertyColors[2]: #pink
-                if self.name == property.propertyName[5]: #O'Brian
+                elif self.name == property.propertyName[5]: #O'Brian
                     return 600
                 elif self.name == property.propertyName[6]: #Norton
                     return 600
                 elif self.name == property.propertyName[7]: #Knox
                     return 750
-            elif self.color == property.propertyColors[3]: #orange
-                if self.name == property.propertyName[8]: #Bonner
+                elif self.name == property.propertyName[8]: #Bonner
                     return 750
                 elif self.name == property.propertyName[9]: #Talbert
                     return 750
                 elif self.name == property.propertyName[10]: #Hochstetter
                     return 750
-            elif self.color == property.propertyColors[4]: #red
-                if self.name == property.propertyName[11]: #Cooke
+                elif self.name == property.propertyName[11]: #Cooke
                     return 900
                 elif self.name == property.propertyName[12]: #Fronczak
                     return 900
                 elif self.name == property.propertyName[13]: #NSC
                     return 950
-            elif self.color == property.propertyColors[5]: #yellow
-                if self.name == property.propertyName[14]: #Ketter
+                elif self.name == property.propertyName[14]: #Ketter
                     return 950
                 elif self.name == property.propertyName[15]: #Slee
                     return 1000
                 elif self.name == property.propertyName[16]: #Furnas
                     return 1000
-            elif self.color == property.propertyColors[6]: #green
-                if self.name == property.propertyName[17]: #The Commons
+                elif self.name == property.propertyName[17]: #The Commons
                     return 1050
                 elif self.name == property.propertyName[18]: #Center for the Arts
                     return 1050
                 elif self.name == property.propertyName[19]: #Alumni Arena
                     return 1100
-            elif self.color == property.propertyColors[7]: #dark blue
-                if self.name == property.propertyName[20]: #Davis
+                elif self.name == property.propertyName[20]: #Davis
                     return 1200
                 elif self.name == property.propertyName[21]: #Jarvis
                     return 1400
